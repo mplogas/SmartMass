0861e4ef2dde83b8f266c45e4ce0982ff0d2ab2e
Marc Plogas <marc.plogas@digitalnatives.de>
2023-09-25 18:25:48 +0200

    usable load cell results

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,13 @@
 // HX711 circuit wiring
 const int LOADCELL_DOUT_PIN = 16;
 const int LOADCELL_SCK_PIN = 4;
-const long KNOWN_WEIGHT = 200.0;
+const long KNOWN_WEIGHT = 100.0;
 boolean FIRST_RUN = true;
 
 HX711 scale;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200);  
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
 }
 
@@ -18,21 +18,25 @@ void loop() {
   if(FIRST_RUN)  {
     if (scale.is_ready()) {
       // taring
-      scale.set_scale();    
       Serial.println("Tare... remove any weights from the scale.");
-      delay(5000);
+      delay(5000);      
+      scale.set_scale();    
       scale.tare();
       Serial.println("Tare done...");
+      delay(1000);
       Serial.print("Place a known weight on the scale...");
       delay(5000);
-      long reading = scale.get_units(20);
+      long reading = scale.get_units(10);
       Serial.print("Result: ");
       Serial.println(reading);
       long calibration = reading/KNOWN_WEIGHT;
-      scale.set_scale(calibration);
       Serial.print("Calibration factor: ");
       Serial.println(calibration);
       FIRST_RUN = false;
+      Serial.println("Tare again... remove any weights from the scale.");
+      delay(5000);
+      scale.set_scale(calibration);
+      scale.tare();
     } 
     else {
       Serial.println("HX711 not found.");
@@ -40,7 +44,7 @@ void loop() {
   } else {
       if (scale.is_ready()) {
         // continuous
-        long reading = scale.read_average(10);
+        long reading = scale.get_units(10);
         Serial.print("HX711 reading -avg(10): ");
         Serial.println(reading);
       }  else {
